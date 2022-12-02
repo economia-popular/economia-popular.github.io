d49a5f32c03b693a992dbcc2fa33b0833e83bbd1
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-02 01:27:29 -0300

    PIB

--- assets/js/pib.js
+++ assets/js/pib.js
@@ -1,5 +1,125 @@
 const pib_variacao = document.getElementById("pib-variacao");
 const pib_per_capta = document.getElementById("pib-per-capta");
+const pib_acumulado = document.getElementById("pib-acumulado");
+const pib_acumulado_anual = document.getElementById("pib-acumulado-anual");
+
+// PIB - Anual
+$.get(
+    "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/pib/pib-acumulado-ano.json",
+    function (data, textStatus, jqXHR) {
+  
+      const dataset = [];
+  
+      var raw = JSON.parse(data);
+
+      raw.data.sort((a, b) => a.ano - b.ano);
+  
+      raw.data.forEach((element) => {
+        console.log(element);
+        temp = {
+          x: element.ano,
+          y: element.valor,
+        };
+        dataset.push(temp);
+      });
+  
+      new Chart(pib_acumulado_anual, {
+          type: "bar",
+          data: {
+            backgroundColor: "#FFFFFF",
+            datasets: [
+              {
+                label: "R$ ",
+                data: dataset,
+                borderWidth: 1,
+                borderColor: "#FFFFFF",
+                backgroundColor: "#FFFFFF"
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            scales: {},
+            layouts: {},
+            plugins: {
+              title: {
+                display: true,
+                text: "PIB - Acumulado de Valor (Trimestral)",
+                color: "#FFFFFF",
+                padding: {
+                  top: 10,
+                  bottom: 30,
+                },
+              },
+              subtitle: {
+                display: true,
+                color: "#FFFFFF",
+                text: raw.unidade_medida,
+              },
+            },
+          },
+        });
+  
+    }
+  );
+
+// PIB - Acumulado
+$.get(
+    "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/pib/pib-acumulado.json",
+    function (data, textStatus, jqXHR) {
+  
+      const dataset = [];
+  
+      var raw = JSON.parse(data);
+
+      raw.data.forEach((element) => {
+        console.log(element);
+        temp = {
+          x: element.periodo,
+          y: element.valor,
+        };
+        dataset.push(temp);
+      });
+  
+      new Chart(pib_acumulado, {
+          type: "bar",
+          data: {
+            backgroundColor: "#FFFFFF",
+            datasets: [
+              {
+                label: "R$ ",
+                data: dataset,
+                borderWidth: 1,
+                borderColor: "#FFFFFF",
+                backgroundColor: "#FFFFFF"
+              },
+            ],
+          },
+          options: {
+            responsive: true,
+            scales: {},
+            layouts: {},
+            plugins: {
+              title: {
+                display: true,
+                text: "PIB - Acumulado de Valor (Trimestral)",
+                color: "#FFFFFF",
+                padding: {
+                  top: 10,
+                  bottom: 30,
+                },
+              },
+              subtitle: {
+                display: true,
+                color: "#FFFFFF",
+                text: raw.unidade_medida,
+              },
+            },
+          },
+        });
+  
+    }
+  );
 
 // PIB - Variação
 $.get(
