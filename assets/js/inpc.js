b4a66ce7ea308b4fa1016b17bb2871a93942eac2
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-01 22:47:14 -0300

    INPC

---
+++ assets/js/inpc.js
@@ -0,0 +1,59 @@
+const inpc_variacao = document.getElementById("inpc-variacao");
+
+// INPC - Variação
+$.get(
+  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/inpc/inpc.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+      console.log(element);
+      temp = {
+        x: element.mes_ano,
+        y: element.variacao_mes,
+      };
+      dataset.push(temp);
+    });
+
+    new Chart(inpc_variacao, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "% em relação ao mês anterior",
+            data: dataset,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {},
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Variação do inpc",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+
+    // Fonte do inpc
+    $("div.fonte-inpc").text(raw.fonte);
+    $("div.atualizacao-inpc").text(raw.data_atualizacao);
+  }
+);
