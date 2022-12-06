50ed0d9bc2f706b0f28e895a4a46023a7b1983c3
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-05 22:20:48 -0300

    IDH - Anos Escola

--- assets/js/idh.js
+++ assets/js/idh.js
@@ -2,6 +2,8 @@ const idh_variacao = document.getElementById("idh-variacao");
 const idh_sexo = document.getElementById("idh-sexo");
 const idh_ev = document.getElementById("idh-ev");
 const idh_ev_sexo = document.getElementById("idh-ev-sexo");
+const idh_as = document.getElementById("idh-as");
+const idh_as_sexo = document.getElementById("idh-as-sexo");
 
 
 // IDH
@@ -266,7 +268,6 @@ $.get(
         });
 
         // Expectativa de Vida - Sexo
-
         const dataset_ev_m = [];
         const dataset_ev_f = [];
 
@@ -364,6 +365,184 @@ $.get(
             },
         });
 
+
+        // Expectativa de Anos na Escola - Geral
+
+        const dataset_as = []
+
+        raw.data.forEach((element) => {
+
+            temp = {
+                x: element.ano_referencia,
+                y: element.expectativa_de_anos_escola,
+            };
+            dataset_as.push(temp);
+        })
+
+        new Chart(idh_as, {
+            type: "bar",
+            data: {
+                backgroundColor: "#FFFFFF",
+                datasets: [
+                    {
+                        label: "Expectativa de Anos na Escola",
+                        data: dataset_as,
+                        borderWidth: 1,
+                        borderColor: "#5D6D2F",
+                        backgroundColor: "#5D6D2F",
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                hover: {
+                    mode: "Anos na Escola",
+                    intersec: false,
+                },
+                scales: {
+                    x: {
+                        display: true,
+                        title: {
+                            display: true,
+                        },
+                        grid: {
+                            color: "#FFFFFF",
+                        },
+                        ticks: {
+                            color: "#FFFFFF",
+                            callback: function (val, index) {
+                                return index % 2 === 0 ? this.getLabelForValue(val) : "";
+                            },
+                            major: {
+                                enabled: true,
+                            },
+                        },
+                    },
+                    y: {
+                        display: true,
+                        color: "#FFFFFF",
+                        grid: {
+                            color: "#FFFFFF",
+                        },
+                        ticks: {
+                            color: "#FFFFFF",
+                        },
+                    },
+                },
+                layouts: {},
+                plugins: {
+                    title: {
+                        display: false,
+                        text: "Expectativa de Anos na Escola",
+                        color: "#FFFFFF",
+                    },
+                    subtitle: {
+                        display: true,
+                        color: "#FFFFFF",
+                        text: "Em anos",
+                    },
+                },
+            },
+        });
+
+
+        // Expectativa de Anos na Escola - Sexo
+        const dataset_as_m = [];
+        const dataset_as_f = [];
+
+        raw.data.forEach((element) => {
+
+            if (element.expectativa_de_vida_feminina > 0 && element.ano_referencia >= 2002) {
+                // Masculino
+                temp_m = {
+                    x: element.ano_referencia,
+                    y: element.expectativa_de_anos_escola_feminina,
+                };
+                dataset_as_m.push(temp_m);
+
+                // Feminino
+                temp_f = {
+                    x: element.ano_referencia,
+                    y: element.expectativa_de_anos_escola_masculina,
+                };
+
+                dataset_as_f.push(temp_f);
+            }
+        });
+
+        new Chart(idh_as_sexo, {
+            type: "bar",
+            data: {
+                backgroundColor: "#FFFFFF",
+                datasets: [
+                    {
+                        label: "Expectativa Escolar Masculino",
+                        data: dataset_as_m,
+                        borderWidth: 1,
+                        borderColor: "#114247",
+                        backgroundColor: "#114247",
+                    },
+
+                    {
+                        label: "Expectativa Escolar Feminino",
+                        data: dataset_as_f,
+                        borderWidth: 1,
+                        borderColor: "#5D6D2F",
+                        backgroundColor: "#5D6D2F",
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                hover: {
+                    mode: "idh",
+                    intersec: false,
+                },
+                scales: {
+                    x: {
+                        display: true,
+                        title: {
+                            display: true,
+                        },
+                        grid: {
+                            color: "#FFFFFF",
+                        },
+                        ticks: {
+                            color: "#FFFFFF",
+                            callback: function (val, index) {
+                                return index % 2 === 0 ? this.getLabelForValue(val) : "";
+                            },
+                            major: {
+                                enabled: true,
+                            },
+                        },
+                    },
+                    y: {
+                        display: true,
+                        color: "#FFFFFF",
+                        grid: {
+                            color: "#FFFFFF",
+                        },
+                        ticks: {
+                            color: "#FFFFFF",
+                        },
+                    },
+                },
+                layouts: {},
+                plugins: {
+                    title: {
+                        display: false,
+                        text: "Expectativa de Anos na Escola",
+                        color: "#FFFFFF",
+                    },
+                    subtitle: {
+                        display: true,
+                        color: "#FFFFFF",
+                        text: raw.unidade_medida,
+                    },
+                },
+            },
+        });
 
 
         // Data Grid
