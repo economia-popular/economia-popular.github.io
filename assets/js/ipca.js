f3a21f55066e5708fc3d6b1421e651c5490c01a2
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-07 23:57:18 -0300

    Melhoria no grafico do IPCA

--- assets/js/ipca.js
+++ assets/js/ipca.js
@@ -7,6 +7,7 @@ $.get(
   "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/ipca/ipca.json",
   function (data, textStatus, jqXHR) {
     const dataset = [];
+    const dataset_acumulado = []
 
     var raw = JSON.parse(data);
 
@@ -19,8 +20,40 @@ $.get(
       dataset.push(temp);
     });
 
+    raw.data.forEach((element) => {
+      
+      temp = {
+        x: element.mes_ano,
+        y: element.acumulado_ano,
+      };
+      dataset_acumulado.push(temp);
+    });
+
+    // Data Grid
+    var columnDefs = [
+      { headerName: "Periodo", field: "mes_ano" },
+      { headerName: "Variação", field: "variacao_mes" },
+      { headerName: "Acumulado Ano", field: "acumulado_ano" }
+    ];
+
+    var gridOptions = {
+      defaultColDef: {
+        flex: 1,
+        sortable: true,
+        filter: true,
+      },
+      columnDefs: columnDefs,
+      rowData: raw.data.reverse(),
+      animateRows: true,
+      accentedSort: true
+    };
+
+    var eGridDiv = document.querySelector('#ipca-grid');
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+
     new Chart(ipca_variacao, {
-      type: "line",
+      type: "bar",
       data: {
         backgroundColor: "#FFFFFF",
         datasets: [
@@ -30,6 +63,15 @@ $.get(
             borderWidth: 1,
             borderColor: "#FFFFFF",
             backgroundColor: "#FFFFFF",
+            type: "line"
+          },
+          {
+            label: "% acumulo anual",
+            data: dataset_acumulado,
+            borderWidth: 1,
+            borderColor: "#5D6D2F",
+            backgroundColor: "#5D6D2F",
+            type: "bar"
           },
         ],
       },
@@ -92,57 +134,57 @@ $.get(
 );
 
 // IPCA - Acumulado
-$.get(
-  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/ipca/ipca.json",
-  function (data, textStatus, jqXHR) {
-    const dataset = [];
+// $.get(
+//   "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/ipca/ipca.json",
+//   function (data, textStatus, jqXHR) {
+//     const dataset = [];
 
-    var raw = JSON.parse(data);
+//     var raw = JSON.parse(data);
 
-    raw.data.forEach((element) => {
+//     raw.data.forEach((element) => {
       
-      temp = {
-        x: element.mes_ano,
-        y: element.acumulado_ano,
-      };
-      dataset.push(temp);
-    });
+//       temp = {
+//         x: element.mes_ano,
+//         y: element.acumulado_ano,
+//       };
+//       dataset.push(temp);
+//     });
 
-    new Chart(ipca_acumulado, {
-      type: "bar",
-      data: {
-        backgroundColor: "#FFFFFF",
-        datasets: [
-          {
-            label: "IPCA Acumulado no ano %",
-            data: dataset,
-            borderWidth: 1,
-            borderColor: "#FFFFFF",
-            backgroundColor: "#FFFFFF",
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        scales: {},
-        layouts: {},
-        plugins: {
-          title: {
-            display: true,
-            text: "Acumulo Anual do IPCA",
-            color: "#FFFFFF",
-            padding: {
-              top: 10,
-              bottom: 30,
-            },
-          },
-          subtitle: {
-            display: true,
-            color: "#FFFFFF",
-            text: raw.unidade_medida,
-          },
-        },
-      },
-    });
-  }
-);
+//     new Chart(ipca_acumulado, {
+//       type: "bar",
+//       data: {
+//         backgroundColor: "#FFFFFF",
+//         datasets: [
+//           {
+//             label: "IPCA Acumulado no ano %",
+//             data: dataset,
+//             borderWidth: 1,
+//             borderColor: "#FFFFFF",
+//             backgroundColor: "#FFFFFF",
+//           },
+//         ],
+//       },
+//       options: {
+//         responsive: true,
+//         scales: {},
+//         layouts: {},
+//         plugins: {
+//           title: {
+//             display: true,
+//             text: "Acumulo Anual do IPCA",
+//             color: "#FFFFFF",
+//             padding: {
+//               top: 10,
+//               bottom: 30,
+//             },
+//           },
+//           subtitle: {
+//             display: true,
+//             color: "#FFFFFF",
+//             text: raw.unidade_medida,
+//           },
+//         },
+//       },
+//     });
+//   }
+// );
