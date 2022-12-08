2e56d27fe694993cb3d7fdb0bfd9ffb0c3e72a99
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-07 21:33:04 -0300

    Selic - Meta

--- assets/js/selic.js
+++ assets/js/selic.js
@@ -1,5 +1,118 @@
 const selic_variacao = document.getElementById("selic-variacao");
 const selic_acumulado = document.getElementById("selic-acumulado");
+const selic_meta = document.getElementById("selic-meta");
+
+// Meta Selic
+$.get(
+  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/selic/selic-meta.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+      temp = {
+        x: element.mes_ano,
+        y: element.valor,
+      };
+      dataset.push(temp);
+    });
+
+    // Data Grid
+    var columnDefs = [
+      { headerName: "Periodo", field: "periodo" },
+      { headerName: "Data Reunião", field: "data_reuniao" },
+      { headerName: "Inicio da Vigência", field: "inicio_vigencia" },
+      { headerName: "Valor", field: "valor" },
+    ];
+
+    var gridOptions = {
+      defaultColDef: {
+        flex: 1,
+        sortable: true,
+        filter: true,
+      },
+      columnDefs: columnDefs,
+      rowData: raw.data.reverse(),
+      animateRows: true,
+      accentedSort: true,
+    };
+
+    var eGridDiv = document.querySelector("#selic-meta-grid");
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+
+    new Chart(selic_meta, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Taxa Selic %",
+            data: dataset,
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              callback: function (val, index) {
+                return index % 2 === 0 ? this.getLabelForValue(val) : "";
+              },
+              major: {
+                enabled: true,
+              },
+            },
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Meta Selic %",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+
+    // Fonte do selic
+    $("div.fonte-selic").text(raw.fonte);
+    $("div.atualizacao-selic").text(raw.data_atualizacao);
+  }
+);
 
 // SELIC - Variação
 $.get(
@@ -10,7 +123,6 @@ $.get(
     var raw = JSON.parse(data);
 
     raw.data.forEach((element) => {
-      
       temp = {
         x: element.mes_ano,
         y: element.valor,
@@ -28,42 +140,42 @@ $.get(
             data: dataset,
             borderWidth: 1,
             borderColor: "#FFFFFF",
-            backgroundColor: "#FFFFFF"
+            backgroundColor: "#FFFFFF",
           },
         ],
       },
       options: {
         responsive: true,
         scales: {
-            x: {
+          x: {
+            display: true,
+            title: {
               display: true,
-              title: {
-                display: true,
-              },
-              grid: {
-                color: "#FFFFFF",
-              },
-              ticks: {
-                color: "#FFFFFF",
-                callback: function (val, index) {
-                  return index % 2 === 0 ? this.getLabelForValue(val) : "";
-                },
-                major: {
-                  enabled: true,
-                },
-              },
             },
-            y: {
-              display: true,
+            grid: {
               color: "#FFFFFF",
-              grid: {
-                color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              callback: function (val, index) {
+                return index % 2 === 0 ? this.getLabelForValue(val) : "";
               },
-              ticks: {
-                color: "#FFFFFF",
-              }
+              major: {
+                enabled: true,
+              },
             },
           },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
         layouts: {},
         plugins: {
           title: {
@@ -90,92 +202,90 @@ $.get(
   }
 );
 
-
 // SELIC - Percentual / Ano
-$.get("https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/selic/selic-percentual-ano.json",
-    function (data, textStatus, jqXHR) {
-      const dataset = [];
-  
-      var raw = JSON.parse(data);
-  
-      raw.data.forEach((element) => {
-        
-        temp = {
-          x: element.mes_ano,
-          y: element.valor,
-        };
-        dataset.push(temp);
-      });
-  
-      new Chart(selic_acumulado, {
-        type: "line",
-        data: {
-          backgroundColor: "#FFFFFF",
-          datasets: [
-            {
-              label: "Taxa Percentual/Ano",
-              data: dataset,
-              borderWidth: 1,
-              borderColor: "#FFFFFF",
-              backgroundColor: "#FFFFFF"
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          scales: {
-            x: {
-              display: true,
-              title: {
-                display: true,
-              },
-              grid: {
-                color: "#FFFFFF",
-              },
-              ticks: {
-                color: "#FFFFFF",
-                callback: function (val, index) {
-                  return index % 2 === 0 ? this.getLabelForValue(val) : "";
-                },
-                major: {
-                  enabled: true,
-                },
-              },
-            },
-            y: {
-              display: true,
-              color: "#FFFFFF",
-              grid: {
-                color: "#FFFFFF",
-              },
-              ticks: {
-                color: "#FFFFFF",
-              }
-            },
+$.get(
+  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/selic/selic-percentual-ano.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+      temp = {
+        x: element.mes_ano,
+        y: element.valor,
+      };
+      dataset.push(temp);
+    });
+
+    new Chart(selic_acumulado, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Taxa Percentual/Ano",
+            data: dataset,
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
           },
-          layouts: {},
-          plugins: {
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
             title: {
               display: true,
-              text: "Acumulo da Selic %",
+            },
+            grid: {
               color: "#FFFFFF",
-              padding: {
-                top: 10,
-                bottom: 30,
+            },
+            ticks: {
+              color: "#FFFFFF",
+              callback: function (val, index) {
+                return index % 2 === 0 ? this.getLabelForValue(val) : "";
+              },
+              major: {
+                enabled: true,
               },
             },
-            subtitle: {
-              display: true,
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
               color: "#FFFFFF",
-              text: raw.unidade_medida,
+            },
+            ticks: {
+              color: "#FFFFFF",
             },
           },
         },
-      });
-  
-      // Fonte do selic
-      $("div.fonte-selic").text(raw.fonte);
-      $("div.atualizacao-selic").text(raw.data_atualizacao);
-    }
-  );
-  
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Acumulo da Selic %",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+
+    // Fonte do selic
+    $("div.fonte-selic").text(raw.fonte);
+    $("div.atualizacao-selic").text(raw.data_atualizacao);
+  }
+);
