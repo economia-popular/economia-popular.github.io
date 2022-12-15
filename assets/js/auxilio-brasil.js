41ebc8c7a0ff4af4a5e499a725fcacb8c77641d3
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-15 17:42:03 -0300

    Auxilio Brasil

---
+++ assets/js/auxilio-brasil.js
@@ -0,0 +1,377 @@
+const auxilio_brasil_variacao = document.getElementById(
+  "auxilio-brasil-variacao"
+);
+const auxilio_brasil_cobertura = document.getElementById(
+  "auxilio-brasil-cobertura"
+);
+const auxilio_brasil_valor = document.getElementById("auxilio-brasil-valor");
+const auxilio_brasil_valor_total = document.getElementById(
+  "auxilio-brasil-valor-total"
+);
+
+$.get(
+  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/sociais/auxilio_brasil_consolido.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+    const dataset_familias = [];
+    const dataset_cobertura = [];
+    const dataset_valor = [];
+    const dataset_valor_total = [];
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+      temp = {
+        x: element.referencia,
+        y: element.familias_cobertura,
+      };
+
+      temp_familias_vulneraveis = {
+        x: element.referencia,
+        y: element.familias_vulneraveis,
+      };
+
+      temp_cobertura = {
+        x: element.referencia,
+        y: element.cobertura_auxilio * 100,
+      };
+
+      temp_valor = {
+        x: element.referencia,
+        y: element.valor_beneficio,
+      };
+
+      temp_valor_total = {
+        x: element.referencia,
+        y: element.valor_total_repassado,
+      };
+
+      dataset.push(temp);
+      dataset_familias.push(temp_familias_vulneraveis);
+      dataset_cobertura.push(temp_cobertura);
+      dataset_valor.push(temp_valor);
+      dataset_valor_total.push(temp_valor_total);
+    });
+
+    // Data Grid
+    var columnDefs = [
+      { headerName: "Periodo", field: "referencia" },
+      { headerName: "Familias Beneficiarias", field: "familias_cobertura" },
+      { headerName: "Familias Cadastradas", field: "familias_vulneraveis" },
+      { headerName: "Cobertura do Auxilio %", field: "cobertura_auxilio" },
+      { headerName: "Valor total repassado", field: "valor_total_repassado" },
+      { headerName: "Valor do beneficio", field: "valor_beneficio" },
+    ];
+
+    var gridOptions = {
+      defaultColDef: {
+        flex: 1,
+        sortable: true,
+        filter: true,
+      },
+      columnDefs: columnDefs,
+      rowData: raw.data.reverse(),
+      animateRows: true,
+      accentedSort: true,
+    };
+
+    var eGridDiv = document.querySelector("#auxilio-brasil-grid");
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+
+    new Chart(auxilio_brasil_variacao, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Familias que recebem o auxilio",
+            data: dataset,
+            borderWidth: 1,
+            borderColor: "#5D6D2F",
+            backgroundColor: "#5D6D2F",
+            type: "bar",
+          },
+          {
+            label: "Numero de familias vulneraveis cadastradas",
+            data: dataset_familias,
+            borderWidth: 1,
+            borderColor: "#114247",
+            backgroundColor: "#114247",
+            type: "bar",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true,
+              },
+            },
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Cobertura do Auxilio Brasil",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+
+    new Chart(auxilio_brasil_cobertura, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Porcentagem de Cobertura do Auxilio Brasil %",
+            data: dataset_cobertura,
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
+            type: "line",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true,
+              },
+            },
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Cobertura do Auxilio Brasil",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+  }
+);
+
+
+$.get(
+    "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/sociais/auxilio_brasil.json",
+    function (data, textStatus, jqXHR) {
+
+      const dataset_valor = [];
+      const dataset_valor_total = [];
+  
+      var raw = JSON.parse(data);
+  
+      raw.data.forEach((element) => {
+  
+        temp_valor = {
+          x: element.referencia,
+          y: element.valor_beneficio,
+        };
+  
+        temp_valor_total = {
+          x: element.referencia,
+          y: element.valor_total_repassado,
+        };
+  
+        dataset_valor.push(temp_valor);
+        dataset_valor_total.push(temp_valor_total);
+      });
+    
+  
+      new Chart(auxilio_brasil_valor, {
+        type: "line",
+        data: {
+          backgroundColor: "#FFFFFF",
+          datasets: [
+            {
+              label: "Valor médio do auxilio R$",
+              data: dataset_valor,
+              borderWidth: 1,
+              borderColor: "#FFFFFF",
+              backgroundColor: "#FFFFFF",
+              type: "line",
+              fill: true,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true,
+              },
+              grid: {
+                color: "#FFFFFF",
+              },
+              ticks: {
+                color: "#FFFFFF",
+                major: {
+                  enabled: true,
+                },
+              },
+            },
+            y: {
+              display: true,
+              color: "#FFFFFF",
+              grid: {
+                color: "#FFFFFF",
+              },
+              ticks: {
+                color: "#FFFFFF",
+              },
+            },
+          },
+          layouts: {},
+          plugins: {
+            title: {
+              display: true,
+              text: "Valor do Auxilio brasil",
+              color: "#FFFFFF",
+              padding: {
+                top: 10,
+                bottom: 30,
+              },
+            },
+            subtitle: {
+              display: true,
+              color: "#FFFFFF",
+              text: raw.unidade_medida,
+            },
+          },
+        },
+      });
+  
+      new Chart(auxilio_brasil_valor_total, {
+        type: "line",
+        data: {
+          backgroundColor: "#FFFFFF",
+          datasets: [
+            {
+              label: "Valor total repassado ao programa R$",
+              data: dataset_valor_total,
+              borderWidth: 1,
+              borderColor: "#FFFFFF",
+              backgroundColor: "#FFFFFF",
+              type: "line",
+              fill: true,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true,
+              },
+              grid: {
+                color: "#FFFFFF",
+              },
+              ticks: {
+                color: "#FFFFFF",
+                major: {
+                  enabled: true,
+                },
+              },
+            },
+            y: {
+              display: true,
+              color: "#FFFFFF",
+              grid: {
+                color: "#FFFFFF",
+              },
+              ticks: {
+                color: "#FFFFFF",
+              },
+            },
+          },
+          layouts: {},
+          plugins: {
+            title: {
+              display: true,
+              text: "Total repassado ao Auxilio Brasil",
+              color: "#FFFFFF",
+              padding: {
+                top: 10,
+                bottom: 30,
+              },
+            },
+            subtitle: {
+              display: true,
+              color: "#FFFFFF",
+              text: raw.unidade_medida,
+            },
+          },
+        },
+      });
+    }
+  );
--- ipca/ipca-detalhado.js
+++ ipca/ipca-detalhado.js
@@ -1,6 +1,9 @@
 const ipca_variacao = document.getElementById("ipca-variacao");
 const ipca_acumulado = document.getElementById("ipca-acumulado");
 
+const ipca15_variacao = document.getElementById("ipca15-variacao");
+const ipca15_acumulado = document.getElementById("ipca15-acumulado");
+
 // IPCA - Variação
 $.get(
   "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/inflacao/inflacao.json",
@@ -198,7 +201,7 @@ $.get(
   }
 );
 
-IPCA - Acumulado
+//IPCA - Acumulado
 $.get(
   "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/ipca/ipca.json",
   function (data, textStatus, jqXHR) {
@@ -237,6 +240,201 @@ $.get(
           title: {
             display: true,
             text: "Acumulo Anual do IPCA",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+  }
+);
+
+
+// IPCA15
+
+$.get(
+  "https://raw.githubusercontent.com/msfidelis/indices-economicos/main/data/inflacao/ipca15.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+    const dataset_acumulado = []
+    const dataset_12_meses = []
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+      
+      temp = {
+        x: element.referencia,
+        y: element.variacao,
+      };
+
+      temp_ano = {
+        x: element.referencia,
+        y: element.acumulado_ano,
+      };
+
+      temp_12_meses = {
+        x: element.referencia,
+        y: element.acumulado_doze_meses,
+      };
+
+      dataset.push(temp);
+      dataset_acumulado.push(temp_ano);
+      dataset_12_meses.push(temp_12_meses);
+    });
+
+    // Data Grid
+    var columnDefs = [
+      { headerName: "Periodo", field: "referencia" },
+      { headerName: "Variação %", field: "ipca_variacao" },
+      { headerName: "Acumulado Ano %", field: "ipca_acumulado_ano" },
+      { headerName: "Acumulado 12 Meses %", field: "ipca_acumulado_doze_meses" }
+    ];
+
+    var gridOptions = {
+      defaultColDef: {
+        flex: 1,
+        sortable: true,
+        filter: true,
+      },
+      columnDefs: columnDefs,
+      rowData: raw.data.reverse(),
+      animateRows: true,
+      accentedSort: true
+    };
+
+    // var eGridDiv = document.querySelector('#ipca15-grid');
+
+    // new agGrid.Grid(eGridDiv, gridOptions);
+
+    new Chart(ipca15_variacao, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "% em relação ao mês anterior",
+            data: dataset.slice(-120), // Ultimos 10 anos
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
+            type: "line"
+          }
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true,
+              },
+            },
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Variação do IPCA15 %",
+            color: "#FFFFFF",
+            padding: {
+              top: 10,
+              bottom: 30,
+            },
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+        },
+      },
+    });
+
+
+    new Chart(ipca15_acumulado, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "% acumulo anual",
+            data: dataset_acumulado, // Ultimos 10 anos
+            borderWidth: 1,
+            borderColor: "#5D6D2F",
+            backgroundColor: "#5D6D2F",
+          },
+          {
+            label: "% acumulado 12 meses",
+            data: dataset_12_meses, // Ultimos 10 anos
+            borderWidth: 1,
+            borderColor: "#114247",
+            backgroundColor: "#114247",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true,
+              },
+            },
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            },
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Acumulado IPCA %",
             color: "#FFFFFF",
             padding: {
               top: 10,
