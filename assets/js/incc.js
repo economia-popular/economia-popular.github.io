6df15335ad6436115304c3020b26e5937c27caf3
Matheus Fidelis <matheus@nanoshots.com.br>
2023-02-18 13:18:14 -0300

    INCC-M e Kaggle das Queimadas

--- assets/js/incc.js
+++ assets/js/incc.js
@@ -1,6 +1,9 @@
 const incc_variacao = document.getElementById("incc-variacao");
 const incc_acumulado = document.getElementById("incc-acumulado");
 
+const incc_m_variacao = document.getElementById("incc-m-variacao");
+const incc_m_acumulado = document.getElementById("incc-m-acumulado");
+
 // INCC - Variação
 $.get(
   "https://economia-popular-delivery-content-indices.s3.amazonaws.com/inflacao/incc.json",
@@ -53,7 +56,7 @@ $.get(
         backgroundColor: "#FFFFFF",
         datasets: [
           {
-            label: "Variação Mensal",
+            label: "Variação Mensal - INCC-DI",
             data: dataset.slice(-120), // Ultimos 10 anos,
             borderWidth: 1,
             borderColor: "#FFFFFF",
@@ -99,7 +102,7 @@ $.get(
         plugins: {
           title: {
             display: true,
-            text: "Evolução Mensal do INCC",
+            text: "Evolução Mensal do INCC-DI",
             color: "#FFFFFF",
           },
           subtitle: {
@@ -122,7 +125,7 @@ $.get(
         backgroundColor: "#FFFFFF",
         datasets: [
           {
-            label: "Acumulo Anual",
+            label: "Acumulo Anual - INCC-DI",
             data: dataset_acumulado.slice(-120), // Ultimos 10 anos,
             borderWidth: 1,
             borderColor: "#FFFFFF",
@@ -168,7 +171,7 @@ $.get(
         plugins: {
           title: {
             display: true,
-            text: "Evolução do Acumulo Anual do INCC",
+            text: "Evolução do Acumulo Anual do INCC - DI",
             color: "#FFFFFF",
           },
           subtitle: {
@@ -190,3 +193,195 @@ $.get(
     $("div.atualizacao-incc").text(raw.data_atualizacao);
   }
 );
+
+
+// INCC - M Variação
+$.get(
+  "https://economia-popular-delivery-content-indices.s3.amazonaws.com/inflacao/incc-m.json",
+  function (data, textStatus, jqXHR) {
+    const dataset = [];
+    const dataset_acumulado = [];
+
+    var raw = JSON.parse(data);
+
+    raw.data.forEach((element) => {
+
+      temp = {
+        x: element.referencia,
+        y: element.variacao,
+      };
+      temp_acumulado = {
+        x: element.referencia,
+        y: element.acumulado_ano,
+      };
+      dataset.push(temp);
+      dataset_acumulado.push(temp_acumulado);
+    });
+
+    new Chart(incc_m_variacao, {
+      type: "line",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Variação Mensal - INCC-M",
+            data: dataset.slice(-120), // Ultimos 10 anos,
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        hover: {
+          mode: 'index',
+          intersec: false
+        },
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true
+              }
+            },
+
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            }
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Evolução Mensal do INCC-M",
+            color: "#FFFFFF",
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+          legend: {
+            labels: {
+              color: "#FFFFFF",
+            }
+          }
+        },
+      },
+    });
+
+    new Chart(incc_m_acumulado, {
+      type: "bar",
+      data: {
+        backgroundColor: "#FFFFFF",
+        datasets: [
+          {
+            label: "Acumulo Anual - INCC-M",
+            data: dataset_acumulado.slice(-120), // Ultimos 10 anos,
+            borderWidth: 1,
+            borderColor: "#FFFFFF",
+            backgroundColor: "#FFFFFF",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        hover: {
+          mode: 'index',
+          intersec: false
+        },
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true
+            },
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+              major: {
+                enabled: true
+              }
+            },
+
+          },
+          y: {
+            display: true,
+            color: "#FFFFFF",
+            grid: {
+              color: "#FFFFFF",
+            },
+            ticks: {
+              color: "#FFFFFF",
+            }
+          },
+        },
+        layouts: {},
+        plugins: {
+          title: {
+            display: true,
+            text: "Evolução do Acumulo Anual do INCC-M",
+            color: "#FFFFFF",
+          },
+          subtitle: {
+            display: true,
+            color: "#FFFFFF",
+            text: raw.unidade_medida,
+          },
+          legend: {
+            labels: {
+              color: "#FFFFFF",
+            }
+          }
+        },
+      },
+    });
+
+    // Data Grid
+    var columnDefs = [
+      { headerName: "Referencia", field: "referencia" },
+      { headerName: "Variação Mensal", field: "variacao" },
+      { headerName: "Acumulado Ano", field: "acumulado_ano" },
+    ];
+
+    var gridOptions = {
+      defaultColDef: {
+        flex: 1,
+        sortable: true,
+        filter: true,
+      },
+      columnDefs: columnDefs,
+      rowData: raw.data.reverse(),
+      animateRows: true,
+      accentedSort: true
+    };
+
+    var eGridDiv = document.querySelector('#incc-m-grid');
+
+    new agGrid.Grid(eGridDiv, gridOptions);
+
+    // Fonte do gini
+    $("div.fonte-incc").text("debit.com.br");
+    $("div.atualizacao-incc").text(raw.data_atualizacao);
+  }
+);
+
